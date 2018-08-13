0e356b43aa08dd5de35ab68801b234cdae90035f
Vadim <coolonce@yandex.ru>
2018-08-13 22:30:13 +0300

    'addFirstPage'

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import $ from './assets/other/libs/jquery/dist/jquery.min.js'
+import Common from './assets/other/js/common.js'
+import Fancybox from './assets/other/libs/fancybox-master/jquery.fancybox.min.js'
+import Mousewhell from './assets/other/libs/jquery-mousewheel-master/jquery.mousewheel.min.js'
 
+require('./assets/other/css/main.css')
+require('./assets/other/libs/fancybox-master/jquery.fancybox.min.css')
+
+
+Vue.use(Common)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
